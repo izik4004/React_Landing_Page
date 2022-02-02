167dfa33e758a0ab2e488af8f6e2e167d30b7f69
izik4004 <izik4004@gmail.com>
2022-02-02 16:12:49 +0100

    changes

---
+++ resources/views/schools/allschools.blade.php
@@ -0,0 +1,23 @@
+@extends('Frontend.master')
+
+@section('title', 'All Schools')
+
+@section('content')
+
+<div class="dashboard-content-one">
+         <!-- Breadcubs Area Start Here -->
+         
+         <!-- Breadcubs Area End Here -->
+         <!-- Admit Form Area Start Here -->
+        
+         <!-- Admit Form Area End Here -->
+         <footer class="footer-wrap-layout1">
+             <div class="copyright">© Copyrights <a href="#">akkhor</a> 2019. All rights reserved. Designed by <a
+                     href="#">PsdBosS</a></div>
+         </footer>
+     </div>
+ </div>
+ <!-- Page Area End Here -->
+</div>
+<!-- jquery-->
+@endsection
