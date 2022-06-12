1098cfb146b02f35ca7a463770e664e2bb0075a7
izik4004 <izik4004@gmail.com>
2022-06-12 18:10:33 +0100

    added images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
+import Download from './components/Download';
+import Experience from './components/Experience';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Hero from './components/Hero';
+import Search from './components/Search';
 
 function App() {
   return (
-    <div className="App">
-     <h1 className='bg-red-500'>zikI</h1>
+    <div className="App text-white overflow-hidden">
+        <Header/>
+        <Hero/>
+        <Experience/>
+        <Search/>
+        <Download/>
+        <Footer/>
     </div>
-  );
+  )
 }
 
 export default App;
